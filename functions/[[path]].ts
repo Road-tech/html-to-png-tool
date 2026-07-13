c571e19552660a94bf71971109b83890728848d9
Road <496814664@qq.com>
2026-07-13 14:31:07 +0800

    Replace _redirects with Cloudflare Pages function

---
+++ functions/[[path]].ts
@@ -0,0 +1,10 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith('/assets/') || url.pathname === '/vite.svg') {
+    return await env.ASSETS.fetch(request);
+  }
+
+  return await env.ASSETS.fetch(new Request('http://localhost/index.html', request));
+}
